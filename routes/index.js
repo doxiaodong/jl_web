1c467de0c1211dd2f92dde1429601af9a7ee94bd
duxiaodong <duxiaodong@youqingcaifu.com>
2014-10-15 15:10:46 +0800

    connect mongoab

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,19 @@ var router = express.Router();
 /* apps start */
 /* home page. */
 router.get('/', function(req, res) {
-	res.render('apps/index', { title: 'jl' });
+	res.render('apps/index', { "title": "jl" });
+});
+
+/* userlist page */
+router.get('/userlist', function(req, res) {
+	var db = req.db;
+	var collection = db.get('user');
+	collection.find({}, {}, function(e, users) {
+		res.render('apps/user/userlist', {
+			"title": "用户列表",
+			"users": users
+		});
+	});
 });
 
 /* apps end */
